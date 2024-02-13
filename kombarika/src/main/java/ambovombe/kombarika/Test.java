c03d2f22067df53f0dc60194ef81369166c7d726
Judi <127020548+judi1968@users.noreply.github.com>
2024-02-13 22:17:54 +0300

    update class

--- kombarika/src/main/java/ambovombe/kombarika/Test.java
+++ kombarika/src/main/java/ambovombe/kombarika/Test.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.HashMap;
 /**
  *
- *  @author Mamisoa
+ *  @author Judi
  */
 public class Test {
 
--- kombarika/src/main/java/ambovombe/kombarika/configuration/main/ViewDetails.java
+++ kombarika/src/main/java/ambovombe/kombarika/configuration/main/ViewDetails.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * @author Mamisoa
+ * @author Judi
  */
 @Getter @Setter
 public class ViewDetails extends Configuration{
